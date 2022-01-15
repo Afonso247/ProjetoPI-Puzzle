7565369cc76045ce044e08703d7392c056f8c566
Afonso Henrique <97695756+Afonso247@users.noreply.github.com>
2022-01-15 11:26:30 -0300

    Add files via upload

---
+++ PI-Puzzle/src/App.svelte
@@ -0,0 +1,27 @@
+<script>
+    import JogoA from "./JogarA.svelte"
+    import JogoB from "./JogarB.svelte"
+    import JogoC from "./JogarC.svelte"
+    import Sobre from "./Sobre.svelte"
+    import Ajuda from "./Ajuda.svelte"
+    import Menu from "./Menu.svelte"
+    import Dificuldade from "./Dificuldade.svelte";
+
+    import { state } from "./State.js"
+</script>
+
+{#if $state === "menu"} 
+    <Menu/>
+{:else if $state === "sobre"}
+    <Sobre/>
+{:else if $state === "ajuda"}
+    <Ajuda/>
+{:else if $state === "dificuldade"}
+    <Dificuldade/>
+{:else if $state === "jogarA"}
+    <JogoA/>
+{:else if $state === "jogarB"}
+    <JogoB/>
+{:else if $state === "jogarC"}
+    <JogoC/>
+{/if}
---
+++ PI-Puzzle/src/Menu.svelte
@@ -0,0 +1,32 @@
+<svelte:head>
+	<link rel="stylesheet" href="/styles/menu.css">
+</svelte:head>
+
+<script>
+	import { state } from './State.js'
+	import { trocarStateDoJogo } from './State.js'
+</script>
+
+<body>
+	<h1>
+		Puzzle - Quebra Cabeça
+	</h1>
+	
+	<button type="button">
+		<div class='menu' on:click={() => trocarStateDoJogo('dificuldade')}>
+			Jogar
+		</div>
+	</button>
+
+	<button type="button">
+		<div class='menu' on:click={() => trocarStateDoJogo('ajuda')}>
+			Ajuda
+		</div>
+	</button>
+	
+	<button type="button">
+		<div class='menu' on:click={() => trocarStateDoJogo('sobre')}>
+			Sobre
+		</div>
+	</button>
+</body>
